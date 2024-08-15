468d66066b7a0169fdbd9833d73e1bda1164d6cb
pickingname <trintrinir@icloud.com>
2024-08-15 11:55:22 +0700

    fix: remove hovering bg effect and add questions

--- components/quiz/questionFourRandomiser.tsx
+++ components/quiz/questionFourRandomiser.tsx
@@ -40,7 +40,7 @@ const QuestionCard: React.FC<{
                   : key === question.correctAnswer
                   ? "bg-green-500 dark:bg-opacity-30 bg-opacity-15 border-green-500 dark:text-white text-black"
                   : "bg-neutral-100 dark:bg-neutral-800"
-                : "bg-neutral-100 hover:bg-neutral-200 dark:bg-neutral-800 dark:hover:bg-neutral-700"
+                : "bg-neutral-100 dark:bg-neutral-800"
             }`}
             onClick={() => !isAnswered && onAnswer && onAnswer(key)}
           >
