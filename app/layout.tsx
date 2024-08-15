0441f21144fed8d07b98d78d90a81bb6c5c98700
pickingname <trintrinir@icloud.com>
2024-08-15 07:27:35 +0700

    fix: questions hover transition bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={outfit.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
