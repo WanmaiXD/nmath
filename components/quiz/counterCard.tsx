002cf3746a81b6e79a5580357fb4c2b9d2ae593c
pickingname <trintrinir@icloud.com>
2024-08-16 07:31:43 +0700

    feat: more top scoreboard implement

--- components/quiz/counterCard.tsx
+++ components/quiz/counterCard.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { CircleX, CircleCheck } from "lucide-react";
 
 export default function CounterCard() {
   return (
     <div>
-        
+      <div className="flex flex-row items-start pt-1 px-4 rounded-md">
+        <div className="flex flex-row items-center">
+          <CircleCheck size={24} />
+          <div className="ml-2">12</div>
+        </div>
+
+        <div className="pl-3 flex flex-row items-center">
+          <CircleX size={24} />
+          <div className="ml-2">24</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
