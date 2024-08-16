bf158946cd9522d4c5194a146509392d02f969f1
WanmaiXD <WanmaiXD@gmail.com>
2024-08-16 20:05:23 +0700

    successfully fixed bugs that both navbar counters are triggered without events and edited package.json

--- components/quiz/counterCard.tsx
+++ components/quiz/counterCard.tsx
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { CircleX, CircleCheck } from "lucide-react";
 import { useScore } from "./scoreContext";
 import { motion, AnimatePresence } from "framer-motion";
 
 export default function CounterCard() {
   const { correctChoices, wrongChoices } = useScore();
+  const prevCorrectChoices = useRef(correctChoices);
+  const prevWrongChoices = useRef(wrongChoices);
 
-  const CounterNumber = ({ number, color }: { number: number, color: string }) => (
-    <div className="ml-2 font-bold text-${color}-600">
+  useEffect(() => {
+    prevCorrectChoices.current = correctChoices;
+  }, [correctChoices]);
+
+  useEffect(() => {
+    prevWrongChoices.current = wrongChoices;
+  }, [wrongChoices]);
+
+  const CounterNumber = ({
+    number,
+    color,
+    shouldAnimate,
+  }: {
+    number: number;
+    color: string;
+    shouldAnimate: boolean;
+  }) => (
+    <div className={`ml-2 font-bold text-${color}-600`}>
       <AnimatePresence mode="wait">
-        <motion.div
-          key={number}
-          initial={{ y: 20, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          exit={{ y: -20, opacity: 0 }}
-          transition={{ duration: 0.3, ease: "easeInOut" }}
-        >
-          {number}
-        </motion.div>
+        {shouldAnimate ? (
+          <motion.div
+            key={number}
+            initial={{ y: 20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -20, opacity: 0 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+          >
+            {number}
+          </motion.div>
+        ) : (
+          <div>{number}</div>
+        )}
       </AnimatePresence>
     </div>
   );
@@ -26,12 +48,20 @@ export default function CounterCard() {
     <div className="flex flex-row items-start pt-1 px-4 rounded-md">
       <div className="flex flex-row items-center">
         <CircleCheck size={20} color="#22c55e" />
-        <CounterNumber number={correctChoices} color="green" />
+        <CounterNumber
+          number={correctChoices}
+          color="green"
+          shouldAnimate={correctChoices !== prevCorrectChoices.current}
+        />
       </div>
 
       <div className="pl-3 flex flex-row items-center">
         <CircleX size={20} color="#dc2626" />
-        <CounterNumber number={wrongChoices} color="red" />
+        <CounterNumber
+          number={wrongChoices}
+          color="red"
+          shouldAnimate={wrongChoices !== prevWrongChoices.current}
+        />
       </div>
     </div>
   );
