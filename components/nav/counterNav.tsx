cf1741c15cb0627758be2aa1a657d18a2a0d33b3
pickingname <trintrinir@icloud.com>
2024-08-24 13:23:42 +0700

    feat: create 404 page

--- components/nav/counterNav.tsx
+++ components/nav/counterNav.tsx
@@ -7,7 +7,7 @@ import NavDropdown from "./navDropdown";
 
 const CounterNav = () => {
   return (
-    <nav className="rounded-md">
+    <nav className="rounded-md animate-all ease-in-out duration-300">
       <div className="mx-auto flex justify-between items-center">
         <Link href={"/"} className="text-black dark:text-white text-2xl">
           nmath.com
